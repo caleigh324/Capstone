51c87200a1c6e898fb40da8c239faef655f9a4a0
caleigh324 <43752178+caleigh324@users.noreply.github.com>
2019-02-19 18:44:18 -0500

    Add files via upload

--- code.php
+++ code.php
@@ -36,19 +36,20 @@
 <input type ="submit" name = "submit_school" value = "Enter">
 </form>
 <?php
-$selected_val = "";
+$selected_school = "";
 if(isset($_POST['submit_school'])){
-  $selected_val = mysqli_real_escape_string($conn, $_POST['school']);
-  echo "You have selected: " .$selected_val;
+  $selected_school = mysqli_real_escape_string($conn, $_POST['school']);
+  echo "You have selected: " .$selected_school;
 }
  ?>
 </div>
 
 <div class ="courses">
-<h3>Please select your courses you took</h3>
+<h3>Please select the courses you took</h3>
 
 <form action = "#" method ="post">
   <?php
+  //$courseName = $row['transfer_course'] . ' ' . $row['transfer_title'];
   //create template
   $sql = "SELECT transfer_course, transfer_title FROM data WHERE school = ? ORDER BY transfer_course ASC";
   //create prepared statement
@@ -60,7 +61,7 @@ if(isset($_POST['submit_school'])){
   } else {
     //bind parameters to the placeholder
     //s is for one single string value
-    mysqli_stmt_bind_param($stmt, "s", $selected_val);
+    mysqli_stmt_bind_param($stmt, "s", $selected_school);
     //run parameters inside database
     mysqli_stmt_execute($stmt);
     $result2 = mysqli_stmt_get_result($stmt);
@@ -68,22 +69,29 @@ if(isset($_POST['submit_school'])){
     while($row = mysqli_fetch_assoc($result2)){
       // inserts all data as array
       //echo "<option>". $row['courses'] ."</option>";
-      echo "<input type='checkbox'>" . $row['transfer_course'] . ' ' . $row['transfer_title'] . "<br>";
+      echo "<input type='checkbox' name ='boxes[]' value = '" . $row['transfer_course'] . ' ' . $row['transfer_title'] ."' >" . $row['transfer_course'] . ' ' . $row['transfer_title'] . "<br>";
           }
   }
+  $selected_course = $row['transfer_course'];
+
+  //value =' . $row['transfer_course'] . '
   ?>
 <br>
 <input type ="submit" name = "submit_courses" value = "Enter">
 </form>
 <?php
-$selected_val2 = "";
-if(isset($_POST['submit_courses'])){
-  $selected_val2 = mysqli_real_escape_string($conn, $_POST['transfer_course']);
-
-  echo "You have selected: " . $selected_val2;
+if(isset($_POST['submit_courses'])){//to run PHP script on submit
+  if(!empty($_POST['boxes'])){
+    // Loop to store and display values of individual checked checkbox.
+    foreach($_POST['boxes'] as $selected){
+      echo "You have selected: " . $selected . "</br>";
+    }
+  }
 }
  ?>
 </div>
+
+
 <div class ="other">
 <h3>If your university or course was not listed, please enter it here</h3>
 <textarea name="message" rows="10" cols="50">
