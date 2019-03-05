8989c040c33a3324e668eef30d1ffb58512fff9f
ccharney <33071669+ccharney@users.noreply.github.com>
2019-03-05 13:23:52 -0500

    Add files via upload

--- code.php
+++ code.php
@@ -4,10 +4,49 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  border-top-right-radius: 18px;
+  border-top-left-radius: 18px;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+</style>
+
+
+
   <title>Arcadia Transfer Equivalency</title>
   <link rel ="stylesheet" type= "text/css" href="style.css">
 </head>
 <body>
+
+<ul>
+  <li><a class="active" href="code.html">Home</a></li>
+  <li><a href="form.html">Add Course</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+
   <?php
   $sql = "SELECT DISTINCT school FROM data;";
   $result = mysqli_query($conn, $sql);
