8f2834422f226a39942a26d3a9dde0a16db0d830
powerdylan <33071677+powerdylan@users.noreply.github.com>
2019-04-10 13:30:25 -0400

    Add files via upload

--- import.php
+++ import.php
@@ -43,43 +43,38 @@
 </body>
 
 <div class = "import">
-<form action = "" method = "post" name = "multipart/form-data">
+<form action = "import.php" method = "post" enctype="multipart/form-data">
 <input type = "file" name = "file" value = ""/>
 <input type ="submit" name = "submit" value = "Import CSV">
 </form>
 <?php
-if ( isset($_POST["submit"]) ) {
+$conn = mysqli_connect("localhost", "root", "", "transfer");
 
-   if ( isset($_FILES["file"])) {
-
-            //if there was an error uploading the file
-        if ($_FILES["file"]["error"] > 0) {
-            echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
-
-        }
-        else {
-                 //Print file details
-             echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-             echo "Type: " . $_FILES["file"]["type"] . "<br />";
-             echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-             echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
-
-                 //if file already exists
-             if (file_exists("upload/" . $_FILES["file"]["name"])) {
-            echo $_FILES["file"]["name"] . " already exists. ";
-             }
-             else {
-                    //Store file in directory "upload" with the name of "uploaded_file.txt"
-            $storagename = "uploaded_file.txt";
-            move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $storagename);
-            echo "Stored in: " . "upload/" . $_FILES["file"]["name"] . "<br />";
+if (isset($_POST["submit"])) {
+    
+    $fileName = $_FILES["file"]["tmp_name"];
+    
+    if ($_FILES["file"]["size"] > 0) {
+        
+        $file = fopen($fileName, "r");
+        
+        while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
+            $sqlInsert = "INSERT into data1 (ORG_CODE_ID,SCHOOL,TRANSFER_COURSE,TRANSFER_TITLE,TRANSFER_SUB_TYPE,ARCADIA_COURSE,ARCADIA_SUB_TYPE,ARCADIA_TITLE,CURRICULAR_REQUIREMENT)
+                   values ('" . $column[0] . "','" . $column[1] . "','" . $column[2] . "','" . $column[3] . "','" . $column[4] . "','". $column[5]."','".$column[6]."','". $column[7]."','". $column[8]."')";
+            $result = mysqli_query($conn, $sqlInsert);
+            
+            if (! empty($result)) {
+                $type = "success";
+                $message = "CSV Data Imported into the Database";
+            } else {
+                $type = "error";
+                $message = "Problem in Importing CSV Data";
             }
         }
-     } else {
-             echo "No file selected <br />";
-     }
+    }
 }
- ?>
+
+?>
 </div>
 </br>
 </html>
